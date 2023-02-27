a7b44e890fa386fc8090974cb567ce6224f8af5b
Sayan Banerjee <75254313+Sayan-sam@users.noreply.github.com>
2023-02-27 14:40:12 +0530

    Commented the approach.

--- question2.java
+++ question2.java
@@ -6,14 +6,20 @@ public class question2 {
 
         Scanner sc = new Scanner(System.in);
         int cases = sc.nextInt();
+
+        /*
+         * The basic logic that we are using is a back tracking method to check if the conditions are satisfied. For this example
+         * we are taking the white beads as +1 and black beads as -1 and whenever we will encounter a prime iteration we will add up all the beads to 
+         * check if the sum is not negative. Negative sum means that there are more black beads than there are white. We will bruteforce through the 
+         * beads and sum up the feasible solutions.
+         */
         for (int i = 0; i < cases; i++) {
             int length = sc.nextInt();
             if (length < 1)
                 System.out.println(0);
-            else if(length == 1)
+            else if (length == 1)
                 System.out.println(2);
-            else
-            {
+            else {
                 System.out.println(valid(0, ' ', length, 0));
             }
 
@@ -22,6 +28,7 @@ public class question2 {
 
     }
     
+    // A utility method to check for prime
     public static boolean isPrime(int n)
     {
         if (n == 1 || n == 0)
@@ -36,6 +43,12 @@ public class question2 {
         return true;
     }
     
+    /*
+     * This is the recursive function where we add one character in the 'color' variable and check if our 'value' variable is negative
+     * or not, if at a prime number the value comes negative, we return that series as not possible, otherwise we continue till the end of the length.
+     * 'index' keeps the track of the currect iteration in the recursion that we will use when we have to check during prime position.
+     * 'value' variable is our checker if the bracelet beads are not running out of the condition
+     */
     public static int valid(int index, Character color, int length ,int value)
     {
         if (color == 'B')
